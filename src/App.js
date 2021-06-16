5a76d73dcda5e1f510bad118be6343d4aa636ab9
Marta Dias <dias.martasousa@gmail.com>
2021-06-16 22:07:43 +0100

    Initiate Dictionary Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App bg-dark">
+      <div className="header">
+        <h1 className="header-title">Dictionary</h1>
+      </div>
+    </div>
+  );
+}
