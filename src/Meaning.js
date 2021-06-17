3c0b802ea75301d88448e3493408b872443ff7b1
Marta Dias <dias.martasousa@gmail.com>
2021-06-17 21:38:58 +0100

    Added meaning component and updated css
    Added the meaning component and dealt with the results from API.

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,5 +2,20 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  return <p>{props.meaning.definitions[0].definition};</p>;
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
