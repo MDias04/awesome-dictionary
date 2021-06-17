2ad2ddafa31685e6d7cf77604cda5de5cdd4fbc0
Marta Dias <dias.martasousa@gmail.com>
2021-06-17 19:03:45 +0100

    Added Axios and Api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { FaSearch } from "react-icons/fa";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
