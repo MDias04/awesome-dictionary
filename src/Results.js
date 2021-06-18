4a024e23595e50d655f08e281ecc0733ef6064c7
Marta Dias <dias.martasousa@gmail.com>
2021-06-19 00:24:28 +0100

    Updated the style of the page

--- src/Results.js
+++ src/Results.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Meaning from "./Meaning";
+import "./styles/Results.css";
 
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <h1 className="word-result">{props.results.word}</h1>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <div className="meaning-result" key={index}>
               {" "}
               <Meaning meaning={meaning} />{" "}
             </div>
